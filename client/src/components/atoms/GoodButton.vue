4e671f73f21813b819f1926bd80ef50e18da6149
soray677 <soray677@outlook.com>
2023-08-12 19:41:38 +0900

    ライフサイクル周りを試してみる

--- client/src/components/atoms/GoodButton.vue
+++ client/src/components/atoms/GoodButton.vue
@@ -1,10 +1,16 @@
 <script setup lang="ts">
-import {ref} from 'vue'
+import {onMounted, onUnmounted, onUpdated, ref} from 'vue'
 
 const count = ref(0)
 function increment() {
     count.value ++
 }
+onMounted(() => {
+    console.log(`the first count is 0? count == 0 : ${count.value == 0}`)
+})
+onUpdated(() => {
+    console.log(`counter updated! current count = ${count.value}`)
+})
 
 </script>
 
